38bad946ac9d8d058ee7cfe69ab492e2fb9735b7
saklainMostake <saklain15-12003@diu.edu.bd>
2022-11-01 23:49:05 +0600

    push

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const cors = require('cors')
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+
+
+// geniusDBuser
+// 7Sx6276I7XaGBKqa
+console.log(process.env.BD_USER)
+console.log(process.env.DB_PASSWORD)
+
+const uri = "mongodb+srv://dbuser1:<password>@cluster0.ckrddue.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+app.get('/', (req, res) =>{
+    res.send('genius car is running on server')
+})
+
+
+
+app.listen(port, (req, res) =>{
+    console.log('api is running on', port)
+})
