0f78e66c4b2fadf32e0d12d8403937460833dfb3
saklainMostake <saklain15-12003@diu.edu.bd>
2022-11-01 23:53:00 +0600

    push

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ app.use(express.json())
 console.log(process.env.BD_USER)
 console.log(process.env.DB_PASSWORD)
 
-const uri = "mongodb+srv://dbuser1:<password>@cluster0.ckrddue.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.BD_USER}:${process.env.DB_PASSWORD}@cluster0.ckrddue.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
