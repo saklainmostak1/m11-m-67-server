9c15a323fdfefd0836feac640e4d4a50a60ea89b
saklainMostake <saklain15-12003@diu.edu.bd>
2022-11-05 04:37:04 +0600

    push

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express()
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const cors = require('cors')
@@ -19,10 +20,35 @@ const uri = `mongodb+srv://${process.env.BD_USER}:${process.env.DB_PASSWORD}@clu
 // console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+ function verifyJWR(req, res, next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        res.status(401).send({message: 'unauthorized message'})
+
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(error, decoded){
+        if(error){
+            res.status(401).send({message: 'unauthorized message'})
+        }
+        req.decoded = decoded;
+        next()
+    })
+
+    // next()
+ }
+
 async function run(){
     try{
         const serviceCollection = client.db('geniusCar').collection('services')
         const orderCollection = client.db('geniusCar').collection('orders')
+
+        app.post('/jwt', async(req, res)=>{
+            const user = req.body
+            console.log(user);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h' })
+            res.send({token})
+        })
 
         app.get('/services', async(req,res)=>{
           const query  = {}
@@ -36,8 +62,9 @@ async function run(){
             const service = await serviceCollection.findOne(query)
             res.send(service)
         })
-        app.get('/orders', async(req, res) =>{
-            console.log(req.query)
+        app.get('/orders', verifyJWR, async(req, res) =>{
+            console.log(req.headers.authorization)
+
             let query = {}
             if(req.query.email){
                 query = {
